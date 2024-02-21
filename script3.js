2043070118ead3aacccba40d4d1520420fd4d72c
laurabriand <labriand@ensc.fr>
2024-02-21 17:17:58 +0100

    Exercice 3

--- script3.js
+++ script3.js
@@ -8,16 +8,36 @@ const esp = ["Malaga", "Madrid", "Barcelone"];
 const ita = ["Naples", "Rome", "Venise"];
 
 const list = document.getElementById("listeDeroulante");
+const maListe = document.getElementById("maliste");
 
 const a = function (event) {
     event.preventDefault();
-    document.createElement("ul");
+    console.log("test");
+    maListe.innerHTML = ""
+
     if (event.target.value == "a") {
+
         fra.forEach(elem => {
-            alert(elem);
+            const l1 = document.createElement("li");
+            l1.textContent = elem;
+            maListe.appendChild(l1);
+        })
+    }
+    else if (event.target.value == "b") {
+        esp.forEach(elem => {
+            const l2 = document.createElement("li");
+            l2.textContent = elem;
+            maListe.appendChild(l2);
+        })
+    }
+    else if (event.target.value == "c") {
+        ita.forEach(elem => {
+            const l3 = document.createElement("li");
+            l3.textContent = elem;
+            maListe.appendChild(l3);
         })
     }
 
 }
 
-form.addEventListener("change", a)
+list.addEventListener("change", a)
