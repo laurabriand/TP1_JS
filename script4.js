3c60912fadbec100ee8d318e5030a0ca437e382e
laurabriand <labriand@ensc.fr>
2024-02-21 18:20:05 +0100

    Exercice 4

--- script4.js
+++ script4.js
@@ -1,1 +1,20 @@
 //EXERCICE 4
+
+const pseudo = document.getElementById("monPseudo");
+const mail = document.getElementById("monEmail");
+const mdp = document.getElementById("monMdp");
+const mdp2 = document.getElementById("monMdp2");
+const couleur = document.getElementById("maCouleur");
+const famille = document.getElementById("listeDeroulante");
+const btn = document.getElementById("bouton");
+
+a = function (event) {
+    event.preventDefault();
+    if (pseudo.textContent.length <= 5) {
+        const err = document.createElement("p");
+        err.textContent = "Veuillez entrer un pseudo plus court";
+    }
+
+}
+
+btn.addEventListener("click", a)
